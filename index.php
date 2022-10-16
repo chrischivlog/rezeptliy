854a1860dbb25d47308d47527c346c765faeec41
Christopher Oneisz <coneisz@chrischi.one>
2022-10-16 13:58:10 +0200

    Frontend v1 Hinzugefügt.
    In dieser Version wurde das Frontend Grafisch erweitert. Diese Version ist nicht mit Mobilen Geräten Kompatibel.

--- index.php
+++ index.php
@@ -1,27 +1,60 @@
-Montag = 
-<?php include('generator.php')?>
+<link rel="stylesheet" href="style.css">
+
+<h2>
+    Was gibt es diese Woche zu Essen?
+</h2>
+
+<button class="reload_list" onClick="window.location.reload();">
+    Neue Gerichte finden
+</button>
 <br>
 
-Dienstag =
-<?php include('generator.php')?>
-<br>
+<div class="box">
+    <h4>Montag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
 
-Mittwoch =
-<?php include('generator.php')?>
-<br>
+<div class="box">
+    <h4>Dienstag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
 
-Donnerstag =
-<?php include('generator.php')?>
-<br>
 
-Freitag =
-<?php include('generator.php')?>
-<br>
+<div class="box">
+    <h4>Mittwoch</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
 
-Samstag =
-<?php include('generator.php')?>
-<br>
+<div class="box">
+    <h4>Donnerstag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
 
-Sonntag =
-<?php include('generator.php')?>
-<br>
+<div class="box">
+    <h4>Freitag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
+
+<div class="box">
+    <h4>Samstag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
+
+<div class="box">
+    <h4>Sonntag</h4>
+    <text class="box_text">
+        <?php include('generator.php') ?>
+    </text>
+</div>
